ee9e9b18676f49d64c3d135f20cfac2ab6ccfe56
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-01-01 21:26:03 +0700

    Add content from the tanawit-frontend-scss project

---
+++ src/components/sections/Experience.vue
@@ -0,0 +1,103 @@
+<template>
+  <section class="section-experience">
+    <h1 class="heading-primary u-margin-bottom-big section-experience__heading">Experience</h1>
+    <div class="section-experience__card">
+      <div class="experience-card">
+        <h3 class="experience-card__position">Software Engineer (Frontend)</h3>
+        <a href="http://central.tech" class="experience-card__company-name">Central Technology</a>
+        <p class="experience-card__duration">November 2019-Present</p>
+        <a href="http://central.tech"><img src="../../assets/img/central-tech.png" alt="Central Tech logo" class="experience-card__company-logo"></a>
+        <p class="experience-card__description">
+          Exploring the frontend development world.
+        </p>
+      </div>
+      <div class="experience-card">
+        <h3 class="experience-card__position">Machine Learning Engineer</h3>
+        <a href="http://kbtg.tech/en" class="experience-card__company-name">Kasikorn Business-Technology Group</a>
+        <p class="experience-card__duration">July 2018-October 2019</p>
+        <a href="http://kbtg.tech/en"><img src="../../assets/img/kbtg.png" alt="KBTG logo" class="experience-card__company-logo"></a>
+        <p class="experience-card__description">
+          My first journey in software development. I worked with Data Scientists to develop
+          machine learning-based services which improved KBank customer experience, boosted
+          revenue, and reduced manual processes. The projects covered various areas such as
+          credit scoring, OCR, and virtual wallet.
+        </p>
+      </div>
+      <div class="experience-card">
+        <h3 class="experience-card__position">Data Scientist Intern</h3>
+        <a href="http://kbtg.tech/en" class="experience-card__company-name">Kasikorn Business-Technology Group</a>
+        <p class="experience-card__duration">June 2017-August 2017</p>
+        <a href="http://kbtg.tech/en"><img src="../../assets/img/kbtg.png" alt="KBTG logo" class="experience-card__company-logo"></a>
+        <p class="experience-card__description">
+          Applied statistics and machine learning knowledge with real-world business problems.
+          Our team identified characteristics of potentially default credit card customers and
+          proposed solutions to prevent credit approval for credit card applicants that obtain
+          such characteristics. We won 1st place in the internship project competition.
+        </p>
+      </div>
+      <div class="experience-card">
+        <h3 class="experience-card__position">Intern</h3>
+        <a href="https://www.krungsrisecurities.com" class="experience-card__company-name">Krungsri Securities</a>
+        <p class="experience-card__duration">June 2016</p>
+        <a href="https://www.krungsrisecurities.com"><img src="../../assets/img/krungsri.jpg" alt="Krungsri Securities logo" class="experience-card__company-logo"></a>
+        <p class="experience-card__description">
+          Analyzed business performance and financial reports of a company of interest and
+          identified its target stock price. I gained a concrete understanding of a capital
+          market and investment strategy.
+        </p>
+      </div>
+      <div class="experience-card">
+        <h3 class="experience-card__position">Sales Professional</h3>
+        <a href="https://www.facebook.com/CBAChula" class="experience-card__company-name">Chulalongkorn Business Administration (CBA)</a>
+        <p class="experience-card__duration">May 2015-July 2015</p>
+        <a href="https://www.facebook.com/CBAChula"><img src="../../assets/img/cba.jpg" alt="CBA logo" class="experience-card__company-logo"></a>
+        <p class="experience-card__description">
+          Identified consumer needs and set up sales booths in different locations in Bangkok.
+          I achieved the sales target within five weeks out of 11 weeks of the program period.
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default {
+  name: 'section-experience',
+};
+</script>
+
+<style scoped lang="scss">
+.section-experience {
+  padding: 10rem;
+  position: relative;
+  background-image: linear-gradient(
+    to right bottom,
+    rgba($color-grey-light-1, .9),
+    rgba(#fff, .9)),
+    url(../../assets/img/experience-bg.jpg);
+  background-position: center 35%;
+  background-size: cover;
+
+  display: grid;
+  grid-template-columns: 1fr minmax(30%, 80rem) 1fr;
+
+  @include respond(medium) {
+    padding: 10rem 5rem;
+  }
+
+  @include respond(small) {
+    padding: 10rem 3rem;
+  }
+
+  &__heading {
+    color: $color-primary;
+    grid-column: 1 / -1;
+  }
+
+  &__card {
+    grid-column: 2 / 3;
+  }
+}
+</style>
---
+++ src/components/sections/Project.vue
@@ -0,0 +1,160 @@
+<template>
+  <section class="section-project">
+    <h1 class="heading-primary u-margin-bottom-small section-project__heading">
+      Personal Projects
+    </h1>
+    <div class="section-project__quote">
+      <p>For the things we have to learn before we can do them, we learn by doing them.</p>
+      <p>- Aristotle, The Nicomachean Ethics -</p>
+    </div>
+    <div class="section-project__card">
+      <div class="project-card">
+        <h3 class="project-card__name u-center-text">Tanawit's Web Profile</h3>
+        <img
+          src="../../assets/img/project-tanawitp.png"
+          alt="Tanawit's Web Profile"
+          class="project-card__img"
+        >
+        <p class="project-card__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices
+          gravida dictum fusce ut. Velit euismod in pellentesque massa.
+        </p>
+        <div class="project-card__footer">
+            <div
+              onclick="window.location='https://tanawitp.me';"
+              class="
+                project-card__footer__double-button
+                project-card__footer__double-button--1
+                u-center-text
+              "
+            >
+              Website
+            </div>
+            <div
+              onclick="window.location='https://github.com/tanawitpat/tanawitp-frontend-sass';"
+              class="
+                project-card__footer__double-button
+                project-card__footer__double-button--2
+                u-center-text
+              "
+            >
+              Source Code
+            </div>
+        </div>
+      </div>
+      <div class="project-card">
+        <h3 class="project-card__name u-center-text">Kiren BBQ</h3>
+        <img src="../../assets/img/project-kiren.png" alt="Kiren BBQ" class="project-card__img">
+        <p class="project-card__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices
+          gravida dictum fusce ut. Velit euismod in pellentesque massa.
+        </p>
+        <div class="project-card__footer">
+            <div onclick="window.location='http://kiren.tanawitp.me';" class="project-card__footer__double-button project-card__footer__double-button--1 u-center-text">
+              Website
+            </div>
+            <div onclick="window.location='http://github.com/tanawitpat/kiren-frontend';" class="project-card__footer__double-button project-card__footer__double-button--2 u-center-text">
+              Source Code
+            </div>
+        </div>
+      </div>
+      <div class="project-card">
+        <h3 class="project-card__name u-center-text">ML Kubernetes</h3>
+        <img
+          src="../../assets/img/project-ml-kubernetes.png"
+          alt="ML Kubernetes"
+          class="project-card__img"
+        >
+        <p class="project-card__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices
+          gravida dictum fusce ut. Velit euismod in pellentesque massa.
+        </p>
+        <div class="project-card__footer">
+            <div
+              onclick="window.location='http://github.com/tanawitpat/ml-kubernetes';"
+              class="project-card__footer__single-button u-center-text"
+            >
+              Source Code
+            </div>
+        </div>
+      </div>
+    </div>
+    <div class="section-project__footer">
+      <button onclick="window.location='https://github.com/tanawitpat'"  type="button" class="btn">
+        Explore more
+      </button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default {
+  name: 'section-project',
+};
+</script>
+
+<style scoped lang="scss">
+.section-project {
+  padding: 10rem 15rem;
+  background-image: linear-gradient(
+    to right bottom,
+    rgba($color-blue-dark, .9),
+    rgba($color-blue-dark, .9)),
+    url(../../assets/img/bg1.jpg);
+
+  @include respond(medium) {
+      padding: 10rem 5rem;
+  }
+
+  @include respond(small) {
+      padding: 10rem 3rem;
+  }
+
+  &__heading {
+      color: #fff;
+  }
+
+  &__quote {
+      color: #fff;
+      text-align: center;
+      font-size: 1.5rem;
+      margin-bottom: 6rem;
+  }
+
+  &__card {
+      margin-bottom: 6rem;
+
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(30rem, 1fr));
+      grid-gap: 5rem;
+  }
+
+  &__footer {
+    text-align: center;
+
+    button.btn {
+      border: none;
+      color: $color-blue-dark;
+      background-color: rgba(#fff, .8);
+      font-size: 1.6rem;
+      font-weight: 600;
+      padding: 1.5rem 2.5rem;
+      text-transform: uppercase;
+      transition: transform .2s, background-color .2s, color .2s;
+      cursor: pointer;
+
+      &:hover,
+      &:active {
+        background-color: rgba(#fff, .05);
+        color: #fff;
+        transform: scale(1.1);
+      }
+    }
+  }
+}
+</style>
