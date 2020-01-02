9b11575456a5a94cec332c2c1aadb3cca1776252
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-01-02 23:25:11 +0700

    Create ExperienceCard

---
+++ src/components/ExperienceCard.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="experience-card">
+    <h3 class="experience-card__position">{{ title }}</h3>
+    <a :href="companyWebsite" class="experience-card__company-name">{{ companyName }}</a>
+    <p class="experience-card__duration">{{ duration }}</p>
+    <a :href="companyWebsite">
+      <img
+        :src="require(`@/assets/${companyLogoPath}`)"
+        :alt="companyName + ' logo'"
+        class="experience-card__company-logo"
+      >
+    </a>
+    <p class="experience-card__description">
+      {{ description }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class EducationCard extends Vue {
+  @Prop() title!: string;
+
+  @Prop() companyName!: string;
+
+  @Prop() companyWebsite!: string;
+
+  @Prop() description!: string;
+
+  @Prop() duration!: string;
+
+  @Prop() companyLogoPath!: string;
+}
+</script>
+
+<style scoped lang="scss">
+.experience-card {
+  position: relative;
+  background-color: rgba(#fff, .7);
+  border-radius: 20px;
+  padding: 3rem 5rem;
+  transition: all .2s;
+
+  &:hover {
+    transform: scale(1.02);
+    box-shadow: .5rem 1rem 1.5rem rgba(black, 0.1);
+  }
+
+  &:not(:last-child) {
+    margin-bottom: 3rem;
+  }
+
+  @include respond(medium) {
+    padding: 2rem 3rem;
+    border-radius: 15px;
+  }
+
+  &__position {
+    color: $color-primary;
+  }
+
+  &__company-name {
+    font-weight: 700;
+    color: $color-secondary;
+    text-decoration: none;
+  }
+
+  &__duration {
+      margin-bottom: 10px;
+  }
+
+  &__company-logo {
+    position: absolute;
+    top: 4rem;
+    right: 4rem;
+
+    @include respond(medium) {
+      display: none;
+    }
+  }
+}
+</style>
--- src/sections/Experience.vue
+++ src/sections/Experience.vue
@@ -2,69 +2,69 @@
   <section class="section-experience">
     <h1 class="heading-primary u-margin-bottom-big section-experience__heading">Experience</h1>
     <div class="section-experience__card">
-      <div class="experience-card">
-        <h3 class="experience-card__position">Software Engineer (Frontend)</h3>
-        <a href="http://central.tech" class="experience-card__company-name">Central Technology</a>
-        <p class="experience-card__duration">November 2019-Present</p>
-        <a href="http://central.tech"><img src="../assets/img/central-tech.png" alt="Central Tech logo" class="experience-card__company-logo"></a>
-        <p class="experience-card__description">
-          Exploring the frontend development world.
-        </p>
-      </div>
-      <div class="experience-card">
-        <h3 class="experience-card__position">Machine Learning Engineer</h3>
-        <a href="http://kbtg.tech/en" class="experience-card__company-name">Kasikorn Business-Technology Group</a>
-        <p class="experience-card__duration">July 2018-October 2019</p>
-        <a href="http://kbtg.tech/en"><img src="../assets/img/kbtg.png" alt="KBTG logo" class="experience-card__company-logo"></a>
-        <p class="experience-card__description">
-          My first journey in software development. I worked with Data Scientists to develop
-          machine learning-based services which improved KBank customer experience, boosted
-          revenue, and reduced manual processes. The projects covered various areas such as
-          credit scoring, OCR, and virtual wallet.
-        </p>
-      </div>
-      <div class="experience-card">
-        <h3 class="experience-card__position">Data Scientist Intern</h3>
-        <a href="http://kbtg.tech/en" class="experience-card__company-name">Kasikorn Business-Technology Group</a>
-        <p class="experience-card__duration">June 2017-August 2017</p>
-        <a href="http://kbtg.tech/en"><img src="../assets/img/kbtg.png" alt="KBTG logo" class="experience-card__company-logo"></a>
-        <p class="experience-card__description">
-          Applied statistics and machine learning knowledge with real-world business problems.
-          Our team identified characteristics of potentially default credit card customers and
-          proposed solutions to prevent credit approval for credit card applicants that obtain
-          such characteristics. We won 1st place in the internship project competition.
-        </p>
-      </div>
-      <div class="experience-card">
-        <h3 class="experience-card__position">Intern</h3>
-        <a href="https://www.krungsrisecurities.com" class="experience-card__company-name">Krungsri Securities</a>
-        <p class="experience-card__duration">June 2016</p>
-        <a href="https://www.krungsrisecurities.com"><img src="../assets/img/krungsri.jpg" alt="Krungsri Securities logo" class="experience-card__company-logo"></a>
-        <p class="experience-card__description">
-          Analyzed business performance and financial reports of a company of interest and
-          identified its target stock price. I gained a concrete understanding of a capital
-          market and investment strategy.
-        </p>
-      </div>
-      <div class="experience-card">
-        <h3 class="experience-card__position">Sales Professional</h3>
-        <a href="https://www.facebook.com/CBAChula" class="experience-card__company-name">Chulalongkorn Business Administration (CBA)</a>
-        <p class="experience-card__duration">May 2015-July 2015</p>
-        <a href="https://www.facebook.com/CBAChula"><img src="../assets/img/cba.jpg" alt="CBA logo" class="experience-card__company-logo"></a>
-        <p class="experience-card__description">
-          Identified consumer needs and set up sales booths in different locations in Bangkok.
-          I achieved the sales target within five weeks out of 11 weeks of the program period.
-        </p>
-      </div>
+      <ExperienceCard
+        title="Software Engineer"
+        companyName="Central Technology"
+        duration="November 2019-Present"
+        companyWebsite="http://central.tech"
+        description="Exploring the frontend development world."
+        companyLogoPath="img/central-tech.png"
+      />
+      <ExperienceCard
+        title="Machine Learning Engineer"
+        companyName="Kasikorn Business-Technology Group"
+        duration="July 2018-October 2019"
+        companyWebsite="http://kbtg.tech/en"
+        description="My first journey in software development. I worked with Data Scientists
+          to develop machine learning-based services which improved KBank customer experience,
+          boosted revenue, and reduced manual processes. The projects covered various areas such as
+          credit scoring, OCR, and virtual wallet."
+        companyLogoPath="img/kbtg.png"
+      />
+      <ExperienceCard
+        title="Data Scientist Intern"
+        companyName="Kasikorn Business-Technology Group"
+        duration="June 2017-August 2017"
+        companyWebsite="http://kbtg.tech/en"
+        description="Applied statistics and machine learning knowledge with real-world business
+          problems. Our team identified characteristics of potentially default credit card customers
+          and proposed solutions to prevent credit approval for credit card applicants that obtain
+          such characteristics. We won 1st place in the internship project competition."
+        companyLogoPath="img/kbtg.png"
+      />
+      <ExperienceCard
+        title="Intern"
+        companyName="Krungsri Securities"
+        duration="June 2016"
+        companyWebsite="https://www.krungsrisecurities.com"
+        description="Analyzed business performance and financial reports of a company of interest
+          and identified its target stock price. I gained a concrete understanding of a capital
+          market and investment strategy."
+        companyLogoPath="img/krungsri.jpg"
+      />
+      <ExperienceCard
+        title="Sales Professional"
+        companyName="Chulalongkorn Business Administration (CBA)"
+        duration="May 2015-July 2015"
+        companyWebsite="https://www.facebook.com/CBAChula"
+        description="Identified consumer needs and set up sales booths in different locations in
+          Bangkok. I achieved the sales target within five weeks out of 11 weeks of the program
+          period."
+        companyLogoPath="img/cba.jpg"
+      />
     </div>
   </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import ExperienceCard from '../components/ExperienceCard.vue';
 
 export default {
   name: 'section-experience',
+  components: {
+    ExperienceCard,
+  },
 };
 </script>
 
