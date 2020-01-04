ec3c996d83d462eead34f07bf9a0ae488a3c5738
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-01-04 21:09:03 +0700

    Create SkillCard

--- src/sections/Skill.vue
+++ src/sections/Skill.vue
@@ -2,119 +2,58 @@
   <section class="section-skills">
     <div class="bg-video">
       <video class="bg-video__content" autoplay muted loop>
-        <source src="../assets/img/Love-Coding.mp4" type="video/mp4">
-        <source src="../assets/img/Love-Coding.webm" type="video/webm">
-          Your browser is not supported!
+        <source src="../assets/img/Love-Coding.mp4" type="video/mp4" />
+        <source src="../assets/img/Love-Coding.webm" type="video/webm" />
+        Your browser is not supported!
       </video>
     </div>
-    <h1 class="heading-primary u-margin-bottom-medium section-skills__heading">Skills</h1>
+    <h1 class="heading-primary u-margin-bottom-medium section-skills__heading">
+      Skills
+    </h1>
     <div class="section-skills__cards">
       <div class="section-skills__cards__card">
         <h3 class="section-skills__cards__card--topic">Frontend Development</h3>
         <div class="section-skills__cards__card--content">
-            <div class="skill-card">
-              <img src="../assets/img/skills/javascript.jpg" alt="" class="">
-              <p>JavaScript</p>
-            </div>
-            <div class="skill-card">
-              <img src="../assets/img/skills/react.png" alt="" class="">
-              <p>React</p>
-            </div>
-            <div class="skill-card">
-              <img src="../assets/img/skills/vue.png" alt="" class="">
-              <p>Vue</p>
-            </div>
-            <div class="skill-card">
-              <img src="../assets/img/skills/html.png" alt="" class="">
-              <p>HTML</p>
-            </div>
-            <div class="skill-card">
-              <img src="../assets/img/skills/css.png" alt="" class="">
-              <p>CSS</p>
-            </div>
-            <div class="skill-card">
-              <img src="../assets/img/skills/sass.png" alt="" class="">
-              <p>SASS</p>
-            </div>
+          <SkillCard iconPath="img/skills/javascript.jpg" name="JavaScript" />
+          <SkillCard iconPath="img/skills/react.png" name="React" />
+          <SkillCard iconPath="img/skills/vue.png" name="Vue" />
+          <SkillCard iconPath="img/skills/html.png" name="HTML" />
+          <SkillCard iconPath="img/skills/css.png" name="CSS" />
+          <SkillCard iconPath="img/skills/sass.png" name="SASS" />
         </div>
       </div>
       <div class="section-skills__cards__card">
         <h3 class="section-skills__cards__card--topic">Backend Development</h3>
         <div class="section-skills__cards__card--content">
-            <div class="skill-card">
-              <img src="../assets/img/skills/go.png" alt="" class="">
-              <p>Go</p>
-            </div>
-            <div class="skill-card">
-              <img src="../assets/img/skills/python.png" alt="" class="">
-              <p>Python</p>
-            </div>
-            <div class="skill-card">
-              <img src="../assets/img/skills/graphql.png" alt="" class="">
-              <p>GraphQL</p>
-            </div>
-            <div class="skill-card">
-              <img src="../assets/img/skills/mysql.png" alt="" class="">
-              <p>MySQL</p>
-            </div>
-            <div class="skill-card">
-              <img src="../assets/img/skills/mongodb.png" alt="" class="">
-              <p>MongoDB</p>
-            </div>
-            <div class="skill-card">
-              <img src="../assets/img/skills/redis.png" alt="" class="">
-              <p>Redis</p>
-            </div>
+          <SkillCard iconPath="img/skills/go.png" name="Go" />
+          <SkillCard iconPath="img/skills/python.png" name="Python" />
+          <SkillCard iconPath="img/skills/graphql.png" name="GraphQL" />
+          <SkillCard iconPath="img/skills/mysql.png" name="MySQL" />
+          <SkillCard iconPath="img/skills/mongodb.png" name="MongoDB" />
+          <SkillCard iconPath="img/skills/redis.png" name="Redis" />
         </div>
       </div>
       <div class="section-skills__cards__card">
         <h3 class="section-skills__cards__card--topic">Cloud Services</h3>
         <div class="section-skills__cards__card--content">
-          <div class="skill-card">
-            <img src="../assets/img/skills/ec2.png" alt="" class="">
-            <p>EC2</p>
-          </div>
-          <div class="skill-card">
-            <img src="../assets/img/skills/s3.png" alt="" class="">
-            <p>S3</p>
-          </div>
-          <div class="skill-card">
-            <img src="../assets/img/skills/route53.png" alt="" class="">
-            <p>Route 53</p>
-          </div>
-          <div class="skill-card">
-            <img src="../assets/img/skills/gke.png" alt="" class="">
-            <p>GKE</p>
-          </div>
-          <div class="skill-card">
-            <img src="../assets/img/skills/gcr.png" alt="" class="">
-            <p>Google Cloud Run</p>
-          </div>
-          <div class="skill-card">
-            <img src="../assets/img/skills/firebase.png" alt="" class="">
-            <p>Firebase</p>
-          </div>
+          <SkillCard iconPath="img/skills/ec2.png" name="EC2" />
+          <SkillCard iconPath="img/skills/s3.png" name="ReS3dis" />
+          <SkillCard iconPath="img/skills/route53.png" name="Route53" />
+          <SkillCard iconPath="img/skills/gke.png" name="GKE" />
+          <SkillCard iconPath="img/skills/gcr.png" name="Google Cloud Run" />
+          <SkillCard iconPath="img/skills/firebase.png" name="Firebase" />
         </div>
       </div>
       <div class="section-skills__cards__card">
         <h3 class="section-skills__cards__card--topic">DevOps</h3>
         <div class="section-skills__cards__card--content">
-          <div class="skill-card">
-            <img src="../assets/img/skills/docker.png" alt="" class="">
-            <p>Docker</p>
-          </div>
-          <div class="skill-card">
-            <img src="../assets/img/skills/jenkins.png" alt="" class="">
-            <p>Jenkins</p>
-          </div>
-          <div class="skill-card">
-            <img src="../assets/img/skills/kubernetes.png" alt="" class="">
-            <p>Kubernetes</p>
-          </div>
-          <div class="skill-card">
-            <img src="../assets/img/skills/robotframework.png" alt="" class="">
-            <p>Robot Framework</p>
-          </div>
+          <SkillCard iconPath="img/skills/docker.png" name="Docker" />
+          <SkillCard iconPath="img/skills/jenkins.png" name="Jenkins" />
+          <SkillCard iconPath="img/skills/kubernetes.png" name="Kubernetes" />
+          <SkillCard
+            iconPath="img/skills/robotframework.png"
+            name="Robot Framework"
+          />
         </div>
       </div>
     </div>
@@ -123,9 +62,13 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import SkillCard from '../components/SkillCard.vue';
 
 export default {
   name: 'section-skill',
+  components: {
+    SkillCard,
+  },
 };
 </script>
 
@@ -133,7 +76,7 @@ export default {
 .section-skills {
   padding: 10rem 15rem;
   position: relative;
-  background-color: rgba($color-blue-dark, .85);
+  background-color: rgba($color-blue-dark, 0.85);
 
   @include respond(medium) {
     padding: 10rem 5rem;
@@ -156,7 +99,7 @@ export default {
     grid-template-columns: repeat(2, 1fr);
 
     @include respond(largest) {
-        grid-template-columns: 1fr;
+      grid-template-columns: 1fr;
     }
 
     &__card {
